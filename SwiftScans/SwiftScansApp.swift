92dd932212d96788ea64c54fe87843cb08e0eaa1
jmestro <jmestro@mac.com>
2022-05-01 10:20:29 -0700

    Initial Commit

---
+++ SwiftScans/SwiftScansApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftScansApp.swift
+//  SwiftScans
+//
+//  Created by Joe Mestrovich on 5/1/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftScansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
